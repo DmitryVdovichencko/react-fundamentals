a6c4dac8f06e326518140b06e67f549309d1449c
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2021-06-21 21:52:11 +0500

    finished basic stylings

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -2,6 +2,7 @@
 // http://localhost:3000/isolated/exercise/05.js
 
 import * as React from 'react'
+import PropTypes from 'prop-types';
 import '../box-styles.css'
 
 // 💰 Use the className for the size and style (backgroundColor) for the color
@@ -12,16 +13,25 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = <div className='box box--small' style={{backgroundColor:'lightblue'}}>small lightblue box</div>
+const mediumBox = <div className='box box--medium' style={{backgroundColor:'pink'}}>medium pink box</div>
+const largeBox = <div className='box box--large' style={{backgroundColor:'orange'}}>large orange box</div>
 
+function Box({size, backgroundColor, children}) {
+	const className = `box box--${size}`, style = { fontStyle:'italic', backgroundColor}
+	return <div {...{className, style, children}} />
+}
+Box.propTypes = {
+	size: PropTypes.string,
+	backgroundColor: PropTypes.string
+}
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+			<Box size='small' backgroundColor='lightblue'>small lightblue box</Box>
+			<Box size='medium' backgroundColor='pink'>medium pink box</Box>
+      <Box size='large' backgroundColor='orange'>large orange box</Box>
+			<Box>Sizeless box</Box>
     </div>
   )
 }
